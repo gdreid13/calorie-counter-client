07e660d4d11782b3cf0a82cead6a151d64d3f177
kidus yilma <kidusyilma@gmail.com>
2020-09-10 20:39:31 -0700

    fixed error

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -3,17 +3,10 @@ import './footer-style.css'
 
 export default class footer extends React.Component{
     render(){
-<<<<<<< HEAD
-     return(
-        <div> 
-        </div>
-     )
-=======
         return(
             <div> 
         </div>
         )
         
->>>>>>> 954ba794b2c71fc65a45fcbe640c8286fe42ce81
     }
 }
--- src/routes/homepage/homepage.js
+++ src/routes/homepage/homepage.js
@@ -2,18 +2,9 @@ import React from 'react';
 import Calender from '../../component/calender/calender'
 import './homepage-style.css'
 export default class homePage extends React.Component{
-<<<<<<< HEAD
-    render(){
-        return(
-
-        <div> 
-        </div>
-        )
-=======
 
     state={
         date:new Date()
->>>>>>> 954ba794b2c71fc65a45fcbe640c8286fe42ce81
     }
     render(){
         return(
