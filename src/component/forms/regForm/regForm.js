dac6bf02b9d0579dbcd575aaf4b1b06d1bf1fc11
DuyLuu90 <58050608+DuyLuu90@users.noreply.github.com>
2020-09-12 16:12:05 -0700

    update login and registration forms

--- src/component/forms/regForm/regForm.js
+++ src/component/forms/regForm/regForm.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import AuthHelperService from '../../../services/AuthHelperService'
+//import AuthHelperService from '../../../services/AuthHelperService'
+import './regForm-style.css'
 
 export default class RegForm extends React.Component {
 
     static defaultProps = {
         onRegistrationSuccess: () => { },
+        handleCancel: ()=>{}
     }
 
     state = { error: null }
 
     handleSubmit = ev => {
         ev.preventDefault()
+        console.log('Form submitted')
+        this.props.onRegistrationSuccess()
+        /*
         const { name, age, gender, user_name, password } = ev.target
-
         this.setState({ error: null })
         AuthHelperService.postUser({
             name: name.value,
@@ -32,39 +36,42 @@ export default class RegForm extends React.Component {
             .catch(res => {
                 this.setState({ error: res.message })
             })
+        */
     }
 
     render() {
         return (
-        <section>
-            <form className='register-form'>
-                <div>
-                    <label for="name">Name</label>
+            <form className='form register-form' onSubmit={this.handleSubmit}>
+                <div className='form_input'>
+                    <label htmlFor="name">Name</label>
                     <input placeholder='Full Name' type="text" name='name' id='name' />
                 </div>
-                <div>
-                    <label for="age">Age</label>
-                    <input placeholder='Age' type="number" name='age' id='age' />
+                <div className='form_input'>
+                    <label htmlFor="age">Age</label>
+                    <input placeholder='Enter your age between 18 to 100' type="number" name='age' id='age' min='18'max='100' />
                 </div>
-                <div>
-                    <label for="gender">Gender</label>
+                <div className='form_input'>
+                    <label htmlFor="username">Email</label>
+                    <input type="text" name='user_name' id='user_name' />
+                </div>
+                <div className='form_input'>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name='password' id='password' />
+                </div>
+                <div className='form_input'>
+                    <label htmlFor="gender">Gender</label>
                     <select id="gender" name="gender">
                         <option value="male">Male</option>
                         <option value="female">Female</option>
                         <option value="other">Other</option>
                     </select>
                 </div>
-                <div>
-                    <label for="username">Email</label>
-                    <input type="text" name='user_name' id='user_name' />
+                <div className='form_control'>
+                    <button type='button' onClick={this.props.handleCancel}>Cancel</button>
+                    <button type='submit'>Register</button>
                 </div>
-                <div>
-                    <label for="password">Password</label>
-                    <input type="password" name='password' id='password' />
-                </div>
-                <button type='submit'>Register</button>
+                
             </form>
-        </section>
         )
     }
 }
