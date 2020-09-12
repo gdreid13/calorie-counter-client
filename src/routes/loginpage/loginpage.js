6db1f9c5e64af4a82f17bb989dde2c3f8c3b95ab
kidus yilma <kidusyilma@gmail.com>
2020-09-12 15:46:32 -0700

    fixed error

--- src/routes/loginpage/loginpage.js
+++ src/routes/loginpage/loginpage.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import './loginpage-style.css'
-export default class loginPage extends React.Component{
-    render(){
-        return(
-
-        <div> 
-        </div>
-        )
-    }
+import './loginpage-style.css';
+export default class loginPage extends React.Component {
+	render() {
+		return <div />;
+	}
 }
