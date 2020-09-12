2ae364f0d0fac1ae33f4eb015209ca06af11735f
tyonek <tyonek.thompson@gmail.com>
2020-09-12 16:06:13 -0400

    fitness tips page

--- src/component/fitness/fitness.js
+++ src/component/fitness/fitness.js
@@ -1,10 +1,25 @@
-import { render } from 'enzyme';
 import React from 'react';
+import './fitness-style.css'
 
 export default class FitnessTipsPage extends React.Component {
 	render() {
 		return (
-			<div />
+			<main className="fitnessTips" >
+				<h1 className="header">Fitness Tips</h1>
+				<div className="imgContainer">
+					<h2 className="fitness__options">Healthy Meal Options</h2>
+					<a target="_blank" href="https://tasty.co/article/hannahloewentheil/clean-eating-meal-plan" rel="noopener noreferrer">
+					<img src="/assets/healthymeal.jpg" alt="Healthy meals"></img>
+					</a>
+				</div>
+				<div className="imgContainer">
+				<h2 className="fitness__options">Free Workouts</h2>
+				<a target="_blank" href="https://www.muscleandstrength.com/workout-routines" rel="noopener noreferrer">
+				<img src="/assets/workouts.jpg" alt="Workouts"></img>
+				</a>
+				</div>
+			</main>
+
 		)
 	}
 }
--- src/routes/landingpage/landingpage.js
+++ src/routes/landingpage/landingpage.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './landingpage-style.css';
-import NavBar from '../../component/navBar/navBar';
-import Footer from '../../component/footer/footer';
+
 
 export default class LandingPage extends React.Component{
     render() {
         return (
             <div className="landing">
-                <NavBar />
                 <h1>myCalories</h1>
                 <h3>Keep track of your calorie intake to stay fit!</h3>
                 <h4>This app will allow you to keep track of your weekly and monthly calorie intake</h4>
-                <img src="/assets/landing.jpg" alt="Landing Page Photo"></img>
-                <Footer />
+                <img src="/assets/landing.jpg" alt="jogger"></img>
             </div>
         )
 
