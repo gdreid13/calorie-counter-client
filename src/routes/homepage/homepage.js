a5b798304802dc0b4dc60097b07840cb538566cf
kidus yilma <kidusyilma@gmail.com>
2020-09-12 16:39:52 -0700

    added date on top of the form

--- src/routes/homepage/homepage.js
+++ src/routes/homepage/homepage.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import CalorieCalendar from '../../component/calendar/calendar';
 import './homepage-style.css';
-import Mealinputform from '../../component/forms/mealInputform/mealInputform'
+import Mealinputform from '../../component/forms/mealInputform/mealInputform';
 
 export default class homePage extends React.Component {
+	state = {
+		date: ''
+	};
 
-    state = {
-        date: new Date()
-    }
+	getSelectedData = async (date) => {
+		const selectedDate = await date;
+		this.setState({
+			date: selectedDate
+		});
+	};
 
-    
-    render() {
-        return (
+	render() {
+		let selectedDate = new Date(this.state.date);
+		console.log(selectedDate.toDateString());
+		return (
+			<div className="home">
+				<h1>My Dashboard</h1>
+				<h2>
+					myCalories / Week = <p className="calorieTotal">9000</p>
+					{}
+				</h2>
+				<h2>
+					myCalories / Month = <p className="calorieTotal">90000</p>
+					{}
+				</h2>
+				<CalorieCalendar getSelectedData={this.getSelectedData} />
 
-            <div className="home">
-                <h1>My Dashboard</h1>
-                <h2>myCalories / Week = <p className="calorieTotal">9000</p>{}</h2>
-                <h2>myCalories / Month = <p className="calorieTotal">90000</p>{}</h2>
-                <CalorieCalendar
-                />
-                <Mealinputform />
-            </div>
+				{(selectedDate.toString() === 'Invalid Date') ? <h2> Select Date </h2> : <h2> {selectedDate.toDateString()} </h2>}
 
-        )
-    }
-
+				<Mealinputform />
+			</div>
+		);
+	}
 }
