22cff5a5ebb6e37ab861f5c6d7df5e8361778cac
DuyLuu90 <58050608+DuyLuu90@users.noreply.github.com>
2020-09-15 13:32:43 -0700

    fixed login form rendering

--- src/routes/loginpage/loginpage.js
+++ src/routes/loginpage/loginpage.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React from 'react'
+import LoginForm from '../../component/forms/loginform/loginform'
+// import { withRouter } from 'react-router-dom'
 import './loginpage-style.css';
+
 export default class loginPage extends React.Component {
-	render() {
-		return <div />;
-	}
-}
+    static defaultProps = {
+        location: {},
+        history: {
+            push: () => {},
+        },
+    }
+
+    handleLoginSuccess = () => {
+        const { history } = this.props
+        history.push('/home')
+    }
+
+    render() {
+        return (
+        <section className="login_page">
+            <header role="banner">
+                <h1>Carb Counter</h1>
+                <h3>Sign in to keep track of your calorie intake</h3>
+            </header>
+            <LoginForm 
+                onLoginSuccess={this.handleLoginSuccess}
+            />
+        </section>
+        )
+    }
+} 
