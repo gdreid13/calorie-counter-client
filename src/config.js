2f4cc4c8fd10b47e936e10363d52292ab1d8b5c0
gdreid13 <gdreid13@gmail.com>
2020-09-22 20:28:45 -0700

    fixing route

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
     API_ENDPOINT: (process.env.NODE_ENV==='production')
                 ? process.env.REACT_APP_API_BASE_URL ||
-                'http://sheltered-ravine-01866.herokuapp.com/api'
+                'https://sheltered-ravine-01866.herokuapp.com/api'
                 : 'http://localhost:8000/api',
 
     API_TOKEN: process.env.REACT_APP_API_TOKEN,
