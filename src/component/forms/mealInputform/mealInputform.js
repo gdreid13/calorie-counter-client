848e47d3d9087ca03c96489bf1dd5969359a2964
tyonek <tyonek.thompson@gmail.com>
2020-10-01 20:07:32 -0400

    fixed

--- src/component/forms/mealInputform/mealInputform.js
+++ src/component/forms/mealInputform/mealInputform.js
@@ -1,50 +1,119 @@
-import React from 'react';
-import LogButton from '../Button'
+import React from 'react'
+import { GeneralApiServices } from '../../../services/api-service'
+import moment from 'moment';
+import './mealInputform-style.css'
+// import LogButton from '../Button'
 
 export default class mealInputform extends React.Component {
 	static defaultProps = {
 		history: {
-			push: () => { }
+			push: () => { },
+			selectedDate: new Date()
 		},
+		currentMealInfo: {},
+		userId:''
 	};
+	
 	state = {
 		isLogged: true,
+		meal:{
+			id:'', alldaycalories:0,
+			breakfast_food:'',breakfast_calories:0,
+			lunch_food:'',lunch_calories:0,
+			dinner_food:'',dinner_calories:0,
+		},
+		error:'',
 	}
 
-	isLogged = (e) => {
+	updateCurrentMeal = () =>{
+		const {currentMealInfo}= this.props
+		const defaultMeal= {
+			id:'', alldaycalories:0,
+			breakfast_food:'',breakfast_calories:0,
+			lunch_food:'',lunch_calories:0,
+			dinner_food:'',dinner_calories:0,
+		}
+		if (currentMealInfo.id) {
+			this.setState({meal: currentMealInfo})
+		}
+		else this.setState({meal: defaultMeal})
+	}
 
+	componentDidMount(){
+		this.updateCurrentMeal()
+	}
+	componentDidUpdate(prevProps){
+		if (this.props.currentMealInfo !== prevProps.currentMealInfo){
+			this.updateCurrentMeal()
+		}
+	}
+	
+	getMealsAndCalories = (e) => {
+		e.preventDefault();
+		const {
+			breakfast_food,
+			breakfast_calories,
+			lunch_food,
+			lunch_calories,
+			dinner_food,
+			dinner_calories } = e.target;
+		
+		const total= Number(breakfast_calories.value)+Number(lunch_calories.value)+Number(dinner_calories.value)
 
+		const MealsAndCalories = {
+			alldaycalories: total,
+			breakfast_food: breakfast_food.value,
+			breakfast_calories: breakfast_calories.value,
+			lunch_food: lunch_food.value,
+			lunch_calories: lunch_calories.value,
+			dinner_food: dinner_food.value,
+			dinner_calories: dinner_calories.value,
+		}
 
-		getMealsAndCalories = (e) => {
-			e.preventDefault();
-			const { breakfast_food, breakfast_calorie, lunch_food, lunch_calorie, dinner_food, dinner_calorie } = e.target;
-			const MealsAndCalories = {
-				BreakFast: {
-					breakfast_food: breakfast_food.value,
-					breakfast_calorie: breakfast_calorie.value
-				},
-				Lunch: {
-					lunch_food: lunch_food.value,
-					lunch_calorie: lunch_calorie.value
-				},
-				Dinner: {
-					dinner_food: dinner_food.value,
-					dinner_calorie: dinner_calorie.value
-				}
-			};
+		//console.log(MealsAndCalories)
+		const {userId,selectedDate,currentMealInfo,onAddMealSuccess} = this.props
+		const {id}= currentMealInfo
+		const date= moment(selectedDate).format('YYYY-MM-DD')
 
-			renderCalorieForm(food, calorie); {
-				const { meal } = this.state
-				return (
-					<div className='meal_entries'>
-						<input className="meal__box" type='text' placeholder={food} name={food}
-							value={meal[`${food}`]} onChange={this.onChange} /><br></br>
-						<input className="meal__box" type='number' placeholder={calorie} name={calorie}
-							value={meal[`${calorie}`]} onChange={this.onChange} />
-					</div>
-				)
+		if (userId) {
+			if(id) {
+				GeneralApiServices.patchItemById('meals',id,MealsAndCalories)
+					.then(meal => {
+						onAddMealSuccess()
+					}).catch(res => console.log(res.message))
+			}
+			else {
+				MealsAndCalories.userid= userId
+				MealsAndCalories.dateofmeal= date;
+				GeneralApiServices.postItem('meals', MealsAndCalories)
+					.then((res) => {
+						onAddMealSuccess()
+					})
+					.catch((res) => {
+						this.setState({ error: res.message });
+						alert(JSON.stringify(this.state.error));
+					});
 			}
 		}
+		else this.setState({error:'You have to login to track your calories'})
+	}
+	
+	onChange= e=>{
+		const {name,value}= e.target
+		//this.setState({[key]:newValue})
+		this.setState({meal:{...this.state.meal,[name]:value}})
+    }
+
+	renderCalorieForm(food,calorie){
+		const {meal}=this.state
+		return (
+			<div className='meal_entries'>
+				<input type='text' placeholder={food} name={food} 
+					value={meal[`${food}`]} onChange={this.onChange}/>
+				<input type='number'placeholder={calorie} name={calorie} 
+					value={meal[`${calorie}`]} onChange={this.onChange}/>
+			</div>
+		)
 	}
 
 	render() {
